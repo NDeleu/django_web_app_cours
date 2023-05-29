62928c7b945cbf94ff698ac43adff089efc12ebe
Nicolas Deleu <n.deleu@hotmail.fr>
2023-05-29 15:45:10 +0200

    second step django

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band, Goodies
 
-# Create your views here.
+
+def hello(request):
+    bands = Band.objects.all()
+    return render(request, 'listings/hello.html', {'bands': bands})
+
+
+def about(request):
+    return render(request, 'listings/about.html')
+
+
+def listings(request):
+    goodies = Goodies.objects.all()
+    return render(request, 'listings/listings.html', {'goodies': goodies})
+
+
+def contact(request):
+    return render(request, 'listings/contact.html')
