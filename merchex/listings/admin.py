b35c17e582935593a6bd7029dc04abf16239437a
Nicolas Deleu <n.deleu@hotmail.fr>
2023-05-29 17:24:12 +0200

    third step django

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from listings.models import Band, Goodies
 
-# Register your models here.
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')
+
+
+class GoodiesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'band')
+
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Goodies, GoodiesAdmin)
